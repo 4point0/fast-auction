cb2546d1946538fa988452933c5aa318750961c7
Harry McKinney <HarryPMcKinney@Gmail.com>
2015-09-06 18:22:57 -0400

    Create index.php
    basic template

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html ng-app>
+<head>
+  <title>rapid fire</title>
+  
+</head>
+
+<body>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+</body>
+
+</html>
